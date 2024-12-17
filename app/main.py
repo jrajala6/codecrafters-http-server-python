98997c459e8f7e3e085dd8e0f5651b587974241f
Jisha <jrajala@uci.edu>
2024-12-16 23:05:25 -0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,10 +11,13 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket, client_address = server_socket.accept() # wait for client
     get_request = client_socket.recv(1024).decode().split()
-    if get_request[1] != "/":
-        client_socket.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n')
-    else:
+    if get_request[1] == "/":
         client_socket.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
+    elif "/echo/" in get_request[1]:
+        echo_str = get_request[1][6:]
+        client_socket.sendall(f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo_str)}\r\n\r\n{echo_str}'.encode())
+    else:
+        client_socket.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n')
 
 if __name__ == "__main__":
     main()
