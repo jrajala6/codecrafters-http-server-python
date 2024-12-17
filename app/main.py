dc05913c7f7acc4c2e28325e5e9d06aee5022b50
Jisha <jrajala@uci.edu>
2024-12-16 22:39:21 -0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket  # noqa: F401
+from sqlite3 import connect
 
 
 def main():
@@ -8,8 +9,12 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    client_socket, client_address = server_socket.accept() # wait for client
+    client_socket.sendall('HTTP/1.1 200 OK\r\n\r\n'.encode())
 
+
+def connection_success(server_socket):
+    server_socket.sendall('HTTP/1.1 200 OK\r\n\r\n'.encode())
 
 if __name__ == "__main__":
     main()
