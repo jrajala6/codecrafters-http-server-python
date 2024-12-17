a4603c265b3de177f6ea9f1f385bbcca87fedfd5
Jisha <jrajala@uci.edu>
2024-12-16 23:09:42 -0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,11 +11,14 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket, client_address = server_socket.accept() # wait for client
     get_request = client_socket.recv(1024).decode().split()
+    print(get_request)
     if get_request[1] == "/":
         client_socket.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
     elif "/echo/" in get_request[1]:
         echo_str = get_request[1][6:]
         client_socket.sendall(f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo_str)}\r\n\r\n{echo_str}'.encode())
+    elif get_request[1] == "/user-agent" and get_request[-2] == "User-Agent:":
+        client_socket.sendall(f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(get_request[-1])}\r\n\r\n{get_request[-1]}'.encode())
     else:
         client_socket.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n')
 
