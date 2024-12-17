4355e4108bddcc9d3a9169b1df921b61a53dd4ba
Jisha <jrajala@uci.edu>
2024-12-16 22:59:47 -0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,8 +10,11 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket, client_address = server_socket.accept() # wait for client
-    print(client_socket.fileno())
-    client_socket.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
+    get_request = client_socket.recv(1024).decode().split()
+    if get_request[1] != "/":
+        client_socket.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n')
+    else:
+        client_socket.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
 
 if __name__ == "__main__":
     main()
